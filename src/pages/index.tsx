2130d27035376c3fc650a87c5581217038d9c32c
Marcus Davi Argolo Ribeiro <marcusdavi@gmail.com>
2021-11-15 16:13:01 -0300

    Create Components - layout, tabela, titulo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,36 @@
+import Layout from "../components/Layout";
+import Tabela from "../components/Tabela";
+import Cliente from "../core/Cliente";
+
 export default function Home() {
+  const clientesMock = [
+    new Cliente("Marcus", 35, "1"),
+    new Cliente("Susi", 34, "2"),
+    new Cliente("Thor", 5, "3"),
+    new Cliente("Daiane", 40, "4"),
+  ];
+
+  function clienteSelecionado(cliente: Cliente) {
+    console.log(cliente.nome);
+  }
+
+  function clienteExcluido(cliente: Cliente) {
+    console.log(cliente.idade);
+  }
+
   return (
-    <div className="bg-gray-200 h-screen">
-      <span>Texto</span>
+    <div
+      className={`flex justify-center items-center h-screen
+      bg-gradient-to-r from-blue-500 to-purple-500
+      text-white`}
+    >
+      <Layout titulo="Cadastro Simples">
+        <Tabela
+          clientes={clientesMock}
+          clienteSelecionado={clienteSelecionado}
+          clienteExcluido={clienteExcluido}
+        />
+      </Layout>
     </div>
-  )
+  );
 }
